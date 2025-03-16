f53a10dffb73dc2a0a98fa810c10daf77c03314f
Haris Begic <haris.begic@bragg.group>
2025-03-16 13:56:32 +0100

    add: mutating data

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,10 +9,19 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
+import { useState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const [selectedCustomer, setSelectedCustomer] = useState('');
+  const [amount, setAmount] = useState('');
+  const [status, setStatus] = useState('');
+
+  // Button is disabled if any input is empty
+  const isDisabled = !selectedCustomer || !amount || !status;
+
   return (
-    <form>
+    <form action={ createInvoice }>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -23,6 +34,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              onChange={(e) => setSelectedCustomer(e.target.value)}
             >
               <option value="" disabled>
                 Select a customer
@@ -51,6 +63,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                onChange={(e) => setAmount(e.target.value)}
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -71,6 +84,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  onChange={(e) => setStatus(e.target.value)}
                 />
                 <label
                   htmlFor="pending"
@@ -86,6 +100,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  onChange={(e) => setStatus(e.target.value)}
                 />
                 <label
                   htmlFor="paid"
@@ -105,7 +120,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         >
           Cancel
         </Link>
-        <Button type="submit">Create Invoice</Button>
+        <Button type="submit" disabled={isDisabled}>Create Invoice</Button>
       </div>
     </form>
   );
