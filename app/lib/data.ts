a6dc2cc082df4a8b8e734b203c63aee82754d05c
Haris Begic <haris.begic@bragg.group>
2025-03-16 14:40:40 +0100

    fix: deploy

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -79,7 +79,7 @@ export async function fetchCardData() {
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
-  query: string,
+  query?: string,
   currentPage: number,
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
@@ -113,7 +113,7 @@ export async function fetchFilteredInvoices(
   }
 }
 
-export async function fetchInvoicesPages(query: string) {
+export async function fetchInvoicesPages(query?: string) {
   try {
     const data = await sql`SELECT COUNT(*)
     FROM invoices
